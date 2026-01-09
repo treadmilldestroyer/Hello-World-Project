a58d0d3bbe2a31d0a212cc6bf64030a74b21edbc
treadmilldestroyer <aaronarnold335@gmail.com>
2026-01-09 13:53:52 -0700

    Adjusted settings and comments.

--- hello_world.py
+++ hello_world.py
@@ -1,19 +1,21 @@
 import tkinter as tk
 
-print("Hello World")
+print("Hello World") #Prints in terminal.
 
+#Structure for a pop-up window
 root = tk.Tk()
-root.title("Retro Hello World")
-root.geometry("400x400")
+root.title("Hello World Pop-up Window")
+root.geometry("400x200")
 root.configure(bg="black")
 
+#Details of the pop-up window
 my_label = tk.Label(
     root,
     text="Hello World!",
-    font=("Courier", 30),
+    font=("Courier", 30, "bold"),
     bg="black",
     fg="#00FF00"
 )
 
-my_label.pack(pady=100)
-root.mainloop()
+my_label.pack(pady=65)
+root.mainloop() #IMPORTANT!!! Keeps window visible and waits for user to close window.
